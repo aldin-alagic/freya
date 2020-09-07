c7db497b1d67650eba10222be449dfb346ba231e
aalagic <aldin_alagic@hotmail.com>
2020-09-07 17:47:06 +0200

    Import home component in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
+import Home from "./components/home";
 
 class App extends Component {
   state = {};
