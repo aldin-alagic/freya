7606c804fb4effbe232a96decd4d48164e6391c0
aalagic <aldin_alagic@hotmail.com>
2020-09-09 09:41:01 +0200

    Create text area field component
    Create <TextArea> component which can be reused every time form is being designed.

---
+++ src/components/common/textArea.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const TextArea = ({
+  name,
+  label,
+  columns,
+  rows,
+  error,
+  styleLabel,
+  styleTextArea,
+  ...rest
+}) => {
+  return (
+    <div className="form-group row mx-0 mb-4">
+      <label htmlFor={name} className={styleLabel}>
+        {label}
+      </label>
+      <textarea
+        {...rest}
+        id={name}
+        name={name}
+        cols={columns}
+        rows={rows}
+        className={styleTextArea}
+      ></textarea>
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default TextArea;
