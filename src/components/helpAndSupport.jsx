2831408b67f3d3fb3d01972f1c2b2f3f1b47a7f6
aalagic <aldin_alagic@hotmail.com>
2020-09-11 01:52:35 +0200

    Create a help and support component
    Create a <HelpAndSupport> component that displays cards with links to FAQ, submit a ticket page, and contact us page.

---
+++ src/components/helpAndSupport.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "./common/card";
+import imgFaq from "../images/faq.svg";
+import imgTicket from "../images/ticket.svg";
+import imgContactUs from "../images/contact_us.svg";
+
+const HelpAndSupport = () => {
+  return (
+    <div>
+      <h1 className="mb-5">Help & Support</h1>
+      <div className="row">
+        <Card
+          image={imgFaq}
+          imageStyle="card-img-top p-3"
+          url={"/faq"}
+          button="FAQ"
+          buttonStyle="btn btn-lg btn-outline-primary stretched-link px-5"
+        />
+        <Card
+          image={imgTicket}
+          imageStyle="card-img-top p-3"
+          url={"/submit-ticket"}
+          button="Submit a ticket"
+          buttonStyle="btn btn-lg btn-outline-primary stretched-link px-5"
+        />
+        <Card
+          image={imgContactUs}
+          imageStyle="card-img-top p-3 pt-4"
+          url={"/contact-us"}
+          button="Contact us"
+          buttonStyle="btn btn-lg btn-outline-primary stretched-link px-5"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HelpAndSupport;
