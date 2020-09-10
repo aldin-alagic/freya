8e55c33ef276429a2ac6bc0e7c51839b4dd43166
aalagic <aldin_alagic@hotmail.com>
2020-09-10 13:15:09 +0200

    Create a component with multiple cards for the home page
    Create a <HomeCards> component that contains multiple <Card> components.
    Component contains cards with information about the application.

---
+++ src/components/homeCards.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "./common/card";
+import img_ask from "../images/questions.svg";
+import img_hire from "../images/interview.svg";
+import img_solutions from "../images/solutions.svg";
+
+const HomeCards = () => {
+  return (
+    <div className="row">
+      <Card
+        title="Solutions"
+        text="Get answers to your issue by searching trough predefined categories. Each solutions has short description with detailed issue description."
+        image={img_solutions}
+        imageStyle="card-img-top h-50 p-3"
+        url={"/newsletter"}
+        button="Find"
+        buttonStyle="btn btn-lg btn-outline-primary stretched-link px-5"
+      />
+      <Card
+        title="Ask for help"
+        text="Can't find your solution? Just ask our validated experts from all around the world."
+        image={img_ask}
+        imageStyle="card-img-top h-50 p-3"
+        url={"/newsletter"}
+        button="Ask"
+        buttonStyle="btn btn-lg btn-outline-primary stretched-link px-5"
+      />
+      <Card
+        title="Hire Expert"
+        text="You need extra support? Hire expert per hour or per issue."
+        image={img_hire}
+        imageStyle="card-img-top h-50 p-3"
+        url={"/newsletter"}
+        button="Hire"
+        buttonStyle="btn btn-lg btn-outline-primary stretched-link px-5"
+      />
+    </div>
+  );
+};
+export default HomeCards;
