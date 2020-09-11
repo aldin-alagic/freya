799e348673c79930486c8c2e1a14b15ffe764ae6
aalagic <aldin_alagic@hotmail.com>
2020-09-11 20:50:57 +0200

    Create a menu component for how it works page
    Create a <HowItWorksMenu> component that contains links for displaying information on how the website works for customers or experts.

---
+++ src/components/howItWorksMenu.jsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+
+const HowItWorksMenu = () => {
+  return (
+    <Fragment>
+      <h1 className="mb-4">What do you want?</h1>
+      <NavLink
+        className="btn btn-outline-primary mr-2 mb-2"
+        to="/how-it-works/customer"
+      >
+        Solution
+      </NavLink>
+      <NavLink
+        className="btn btn-outline-primary mb-2"
+        to="/how-it-works/expert"
+      >
+        Work
+      </NavLink>
+      <hr className="my-5" />
+    </Fragment>
+  );
+};
+
+export default HowItWorksMenu;
