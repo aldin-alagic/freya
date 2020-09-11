117d4947fd6b0854cd5dc35b8b55db583450496a
aalagic <aldin_alagic@hotmail.com>
2020-09-11 10:45:19 +0200

    Create a user agreement component
    Create a <UserAgreement> component which displays information about the user agreement.

---
+++ src/components/userAgreement.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const UserAgreement = () => {
+  return (
+    <div className="w-100">
+      <h1>User Agreement</h1>
+      <p className="text-secondary text-justify">
+        This Agreement was last modified on 01 Dec 2020. This User Agreement
+        describes the terms and conditions which you accept by using our Website
+        or our Services. We have incorporated by reference some linked
+        information.
+      </p>
+      <h4>Our privacy obligations</h4>
+      <p className="text-justify">
+        Freelancer.com ("Freelancer") is governed by the Australian Privacy
+        Principles (APPs) under the Privacy Act 1988 (Cth). The APPs regulate
+        how personal information is handled by Freelancer.com.
+        <br />
+        <br />
+        'Personal information' means information or an opinion about an
+        identified individual, or an individual who is reasonably identifiable.
+        Freelancer's Privacy Policy applies to personal information collected
+        and/or held by Freelancer. This Privacy Policy also explains how we
+        process 'personal data' about people in the European Union (EU), as
+        required under the General Data Protection Regulation (GDPR). We will
+        review this policy regularly, and we may update it from time to time.
+        The types of personal information we collect and hold We collect
+        personal information about our users in order provide our products,
+        services, and customer support. Our products, services, and customer
+        support are provided through many platforms including but not limited
+        to: websites, phone apps, email, and telephone. The specific platform
+        and product, service, or support you interact with may affect the
+        personal data we collect.
+        <br />
+        <br />
+        Not all information requested, collected, and processed by us is
+        "Personal Information" as it does not identify you as a specific natural
+        person. This will include majority of "User Generated Content" that you
+        provide us with the intention of sharing with other users. Such
+        "Non-Personal Information" is not covered by this privacy policy.
+        However, as non-personal information may be used in aggregate or be
+        linked with existing personal information; when in this form it will be
+        treated as personal information. As such, this privacy policy will list
+        both types of information for the sake of transparency. In some
+        situation users may provide us with personal information without us
+        asking for it, or through means not intended for the collection of
+        particular types of information. Whilst we may take reasonable steps to
+        protect this data, the user will have bypassed our systems, processes,
+        and control and thus the information provided will not be governed by
+        this privacy policy. In some situations users may provide us personal
+        information over platforms that are outside our control; for example
+        through social media or forums. Whilst any information collected by us
+        is governed by this Privacy Policy, the platform by which it was
+        communicated will be governed by its own Privacy Policy. How we collect
+        personal information Information that you specifically give us While you
+        use our products and services you may be asked to provide certain types
+        of personal information. This might happen through our website,
+        applications, online chat systems, telephone, paper forms, or in-person
+        meetings. We will give you a Collection Notice at the time, to explain
+        how we will use the personal information we are asking for. The notice
+        may be written or verbal.
+      </p>
+    </div>
+  );
+};
+
+export default UserAgreement;
