d5343ab6509cf2e09c3fae7604dcd0ddd61c06aa
aalagic <aldin_alagic@hotmail.com>
2020-09-11 20:56:10 +0200

    Create a component that contains cards that display the advantages of using the application for customers
    Create a <CustomerSmallCards> component for displaying the advantages of using the application for customers on <SmallCard> components.

---
+++ src/components/customerSmallCards.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import SmallCard from "./common/smallCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheckCircle,
+  faPodcast,
+  faUserFriends,
+} from "@fortawesome/free-solid-svg-icons";
+
+const CustomerSmallCards = () => {
+  return (
+    <div className="row">
+      <div className="col-xl-4 col-sm-6 col-12">
+        <SmallCard
+          title="Validated solutions"
+          text="Each solution is validated by field expert or user."
+          icon={
+            <FontAwesomeIcon
+              className="text-primary"
+              icon={faCheckCircle}
+              size="2x"
+            />
+          }
+        />
+      </div>
+      <div className="col-xl-4 col-sm-6 col-12">
+        <SmallCard
+          title="Direct access to experts"
+          text="Chance to contact experts from all kinds of professions."
+          icon={
+            <FontAwesomeIcon
+              className="text-primary"
+              icon={faPodcast}
+              size="2x"
+            />
+          }
+        />
+      </div>
+      <div className="col-xl-4 col-sm-6 col-12">
+        <SmallCard
+          title="Guided support by experts"
+          text="Remote support by hour or by issue"
+          icon={
+            <FontAwesomeIcon
+              className="text-primary"
+              icon={faUserFriends}
+              size="2x"
+            />
+          }
+        />
+      </div>
+    </div>
+  );
+};
+export default CustomerSmallCards;
