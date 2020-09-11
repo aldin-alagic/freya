30f6ff08db05897efe63de4aea27a08a2fddd848
aalagic <aldin_alagic@hotmail.com>
2020-09-11 21:10:36 +0200

    Add titles in <HomeSmallCards> component

--- src/components/homeSmallCards.jsx
+++ src/components/homeSmallCards.jsx
@@ -5,7 +5,9 @@ import ExpertSmallCards from "./expertSmallCards";
 const HomeSmallCards = () => {
   return (
     <Fragment>
+      <h3 className="col-12 text-center mb-4">What's great about it?</h3>
       <CustomerSmallCards />
+      <h3 className="col-12 text-center my-4">Why should experts use it?</h3>
       <ExpertSmallCards />
     </Fragment>
   );
