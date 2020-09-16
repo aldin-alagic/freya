7d5566d52b3363ef25d3592e76c8d8397abd4147
aalagic <aldin_alagic@hotmail.com>
2020-09-16 16:57:45 +0200

    Make design changes to the <FAQ> component
    Make following design changes to the <FAQ> component:
    - Change button sizes
    - Change <DropDownCard> property names

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -5,8 +5,8 @@ const FAQ = () => {
   return (
     <div className="w-100">
       <h1 className="mb-5">Frequently Asked Questions</h1>
-      <button className="btn btn-primary mr-2 mb-2">Customer</button>
-      <button className="btn btn-outline-primary mb-2">Expert</button>
+      <button className="btn btn-primary col-md-2 mr-2 mb-2">Customer</button>
+      <button className="btn btn-outline-primary col-md-2 mb-2">Expert</button>
       <section
         className="accordion-section clearfix mt-4"
         aria-label="Question Accordions"
@@ -19,23 +19,27 @@ const FAQ = () => {
         >
           <DropDownCard
             id="0"
-            question="What is Find Ask Hire?"
-            answer="Find Ask Hire is a online platform which helps users to solve their problems. You can find solutions by searching them or by challagning our experts by creating a post with description of a problem. In case  problem on platform. In case you still need help you can always hire one of our filed experts for extra help."
+            title="What kind of payment is possible?"
+            content="Find Ask Hire is a online platform which helps users to solve their problems. You can find solutions by searching them or by challagning our experts by creating a post with description of a problem. In case  problem on platform. In case you still need help you can always hire one of our filed experts for extra help."
+            titleStyle="collapsed drop-down-card-faq"
           />
           <DropDownCard
             id="1"
-            question="What are tokens?"
-            answer="Find Ask Hire is a online platform which helps users to solve their problems. You can find solutions by searching them or by challagning our experts by creating a post with description of a problem. In case  problem on platform. In case you still need help you can always hire one of our filed experts for extra help."
+            title="What are tokens?"
+            content="Find Ask Hire is a online platform which helps users to solve their problems. You can find solutions by searching them or by challagning our experts by creating a post with description of a problem. In case  problem on platform. In case you still need help you can always hire one of our filed experts for extra help."
+            titleStyle="collapsed drop-down-card-faq"
           />
           <DropDownCard
             id="2"
-            question="What kind of payment is possible?"
-            answer="Find Ask Hire is a online platform which helps users to solve their problems. You can find solutions by searching them or by challagning our experts by creating a post with description of a problem. In case  problem on platform. In case you still need help you can always hire one of our filed experts for extra help."
+            title="What kind of payment is possible?"
+            content="Find Ask Hire is a online platform which helps users to solve their problems. You can find solutions by searching them or by challagning our experts by creating a post with description of a problem. In case  problem on platform. In case you still need help you can always hire one of our filed experts for extra help."
+            titleStyle="collapsed drop-down-card-faq"
           />
           <DropDownCard
             id="3"
-            question="How do I create a challange?"
-            answer="Find Ask Hire is a online platform which helps users to solve their problems. You can find solutions by searching them or by challagning our experts by creating a post with description of a problem. In case  problem on platform. In case you still need help you can always hire one of our filed experts for extra help."
+            title="What kind of payment is possible?"
+            content="Find Ask Hire is a online platform which helps users to solve their problems. You can find solutions by searching them or by challagning our experts by creating a post with description of a problem. In case  problem on platform. In case you still need help you can always hire one of our filed experts for extra help."
+            titleStyle="collapsed drop-down-card-faq"
           />
         </div>
       </section>
