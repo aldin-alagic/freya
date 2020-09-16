ad25912915758e9cc29b6eb9817be82329c25af6
aalagic <aldin_alagic@hotmail.com>
2020-09-16 16:47:54 +0200

    Change <DropDownCard> component properties
    Change <DropDownCard> component property names so that the component is reusable (new names make more sense in different cases):
    - question to title
    - answer to content
    Add titleStyle property for adding CSS classes to the card title.

--- src/components/common/dropDownCard.jsx
+++ src/components/common/dropDownCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const DropDownCard = ({ id, question, answer }) => {
+const DropDownCard = ({ id, title, titleStyle, content }) => {
   const idHeading = "heading" + id;
   const idCollapse = "collapse" + id;
 
@@ -9,7 +9,7 @@ const DropDownCard = ({ id, question, answer }) => {
       <div className="panel-heading p-3 mb-3" role="tab" id={idHeading}>
         <h3 className="panel-title mb-0">
           <a
-            className="collapsed"
+            className={titleStyle}
             role="button"
             title=""
             data-toggle="collapse"
@@ -18,7 +18,7 @@ const DropDownCard = ({ id, question, answer }) => {
             aria-expanded="true"
             aria-controls={idCollapse}
           >
-            {question}
+            {title}
           </a>
         </h3>
       </div>
@@ -28,9 +28,7 @@ const DropDownCard = ({ id, question, answer }) => {
         role="tabpanel"
         aria-labelledby={idHeading}
       >
-        <div className="panel-body px-3 mb-4 text-justify">
-          <p>{answer}</p>
-        </div>
+        <div className="panel-body px-3 mb-4 text-justify">{content}</div>
       </div>
     </div>
   );
