130a63d8d8f0c747b71c8dacf897064326a594cb
aalagic <aldin_alagic@hotmail.com>
2020-09-20 16:11:16 +0200

    Create a component for selecting a energy system part which is then included in a solution
    Create a <EnergySystem> component for displaying energy system parts that the user can include
    in the search for a solution. Each option is mapped from a list of options to <Radio> components.

---
+++ src/components/findSolution/advanced/energySystem.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Radio from "./../../common/radio";
+
+const EnergySystem = () => {
+  const options = [
+    { value: 1, label: "Heather" },
+    { value: 2, label: "Air Conditioner" },
+    { value: 3, label: "Fuel cell system" },
+    { value: 4, label: "Battery/Capacitor" },
+    { value: 5, label: "Power control system" },
+    { value: 6, label: "Drive train system" },
+  ];
+
+  return (
+    <div className="card mb-4">
+      <div className="card-header bg-light">Energy system</div>
+      <div className="card-body row justify-content-start px-5">
+        {options.map((option) => (
+          <Radio
+            key={"energy-system-" + option.value}
+            id={"energy-system-" + option.value}
+            name="energy-system"
+            label={option.label}
+            value={option.value}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EnergySystem;
