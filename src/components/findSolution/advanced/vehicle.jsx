7afb11338ea2f206e83e954b8a994977edbd4562
aalagic <aldin_alagic@hotmail.com>
2020-09-20 16:41:56 +0200

    Create a component for selecting a vehicle for which the user is trying to find a solution

---
+++ src/components/findSolution/advanced/vehicle.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import Select from "./../../common/select";
+import Radio from "../../common/radio";
+
+const Vehicle = () => {
+  const fuelTypeOptions = [
+    { value: 1, label: "Petrol" },
+    { value: 2, label: "Diesel" },
+    { value: 3, label: "Electric" },
+    { value: 4, label: "Ethanol (FFV, E85, etc.)" },
+    { value: 5, label: "Hybrid (Petrol/Electric)" },
+    { value: 6, label: "Hybrid (Diesel/Electric)" },
+    { value: 7, label: "Hydrogen" },
+    { value: 8, label: "Natural gas" },
+    { value: 9, label: "Other" },
+  ];
+
+  const transmitionOptions = [
+    { value: 1, label: "Manual" },
+    { value: 2, label: "Semi-automatic" },
+    { value: 3, label: "Automatic" },
+  ];
+
+  return (
+    <div className="animate__animated animate__fadeInLeft">
+      <div className="card mb-4">
+        <div className="card-header bg-light">Brand, Model, Version</div>
+        <div className="card-body row justify-content-center">
+          <div className="col-md-3">
+            <Select
+              name="brand"
+              label="Brand"
+              options={[
+                { id: 0, text: "Mercedes" },
+                { id: 1, text: "BMW" },
+                { id: 2, text: "Audi" },
+              ]}
+            />
+          </div>
+
+          <div className="col-md-3">
+            <Select
+              name="model"
+              label="Model"
+              options={[
+                { id: 0, text: "C200" },
+                { id: 1, text: "E220" },
+                { id: 2, text: "S230" },
+              ]}
+            />
+          </div>
+
+          <div className="col-md-3">
+            <Select
+              name="version"
+              label="Version"
+              options={[{ id: 0, text: "AMG" }]}
+            />
+          </div>
+
+          <div className="align-self-center mt-3">
+            <FontAwesomeIcon
+              className="text-danger m-1"
+              icon={faTimes}
+              size="2x"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="card mb-4">
+        <div className="card-header bg-light">Fuel type</div>
+        <div className="card-body row justify-content-between px-5">
+          {fuelTypeOptions.map((option) => (
+            <Radio
+              key={"fuel-type-" + option.value}
+              id={"fuel-type-" + option.value}
+              name="fuel-type"
+              label={option.label}
+              value={option.value}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="card mb-4">
+        <div className="card-header bg-light">Transmition</div>
+        <div className="card-body row justify-content-around px-5">
+          {transmitionOptions.map((option) => (
+            <Radio
+              key={"transmition-type-" + option.value}
+              id={"transmition-type-" + option.value}
+              name="transmition-type"
+              label={option.label}
+              value={option.value}
+            />
+          ))}
+        </div>
+      </div>
+      <Link
+        className="btn btn-primary col-2 m-1"
+        to="/find-solution/advanced/issue-type"
+      >
+        Next
+      </Link>
+    </div>
+  );
+};
+
+export default Vehicle;
