3be8669d9ab8b9f2256601b26d177789ce7d2138
aalagic <aldin_alagic@hotmail.com>
2020-09-20 16:13:01 +0200

    Create a component for selecting a interior part which is then included in a solution
    Create a <Interior> component for displaying interior parts that the user can include
    in the search for a solution. Each option is mapped from a list of options to <Radio> components.

---
+++ src/components/findSolution/advanced/interior.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Radio from "../../common/radio";
+
+const Interior = () => {
+  const options = [
+    { value: 1, label: "Cockpit module" },
+    { value: 2, label: "Instrument switch" },
+    { value: 3, label: "Instrument switch" },
+    { value: 4, label: "Airbag system" },
+    { value: 5, label: "Interior trim" },
+    { value: 6, label: "Instrumental panel" },
+  ];
+
+  return (
+    <div className="card mb-4">
+      <div className="card-header bg-light">Interior</div>
+      <div className="card-body row justify-content-start px-5">
+        {options.map((option) => (
+          <Radio
+            key={"interior-" + option.value}
+            id={"interior-" + option.value}
+            name="interior"
+            label={option.label}
+            value={option.value}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Interior;
