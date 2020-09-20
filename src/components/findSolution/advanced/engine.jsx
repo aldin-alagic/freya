3c7f1ba483f3a1d59d4789b194b934a3b955a9ea
aalagic <aldin_alagic@hotmail.com>
2020-09-20 16:12:26 +0200

    Create a component for selecting an engine part which is then included in a solution
    Create a <Engine> component for displaying engine parts that the user can include
    in the search for a solution. Each option is mapped from a list of options to <Radio> components.

---
+++ src/components/findSolution/advanced/engine.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Radio from "./../../common/radio";
+
+const Engine = () => {
+  const options = [
+    { value: 1, label: "Air/Fuel management" },
+    { value: 2, label: "Fuel handling and Evapo system" },
+    { value: 3, label: "Turbo/Supercharger" },
+    { value: 4, label: "Exhaust system" },
+    { value: 5, label: "Engine control system" },
+    { value: 6, label: "Ignition system" },
+    { value: 7, label: "Engine electric system" },
+    { value: 8, label: "Diesel injection system" },
+    { value: 9, label: "Valvetrain" },
+    { value: 10, label: "Engine cooling system" },
+    { value: 11, label: "Gasoline fuel injection system" },
+    { value: 12, label: "Engine lubricating system" },
+  ];
+
+  return (
+    <div className="card mb-4">
+      <div className="card-header bg-light">Engine</div>
+      <div className="card-body row justify-content-start px-5">
+        {options.map((option) => (
+          <Radio
+            key={"engine-" + option.value}
+            id={"engine-" + option.value}
+            name="engine"
+            label={option.label}
+            value={option.value}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Engine;
