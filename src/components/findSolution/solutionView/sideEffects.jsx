30393dd135145fa9ac742079abd5b70dc5147f2b
aalagic <aldin_alagic@hotmail.com>
2020-09-24 21:47:43 +0200

    Create a <SideEffects> component for displaying information on the solution side effects on view solution page
    Create a <SideEffects> component  that displays the possible side effects of the solution on view solution page.

---
+++ src/components/findSolution/solutionView/sideEffects.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SideEffects = () => {
+  return (
+    <div>
+      <h5 className="text-primary bg-light p-2">Side effects</h5>
+      <div className="row justify-content-start px-4 mb-2">
+        <p className="text-justify">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec
+          posuere lorem. Sed interdum facilisis ornare. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Fusce nec posuere lorem. Sed
+          interdum facilisis ornare. Sed interdum facilisis ornare. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Fusce nec posuere lorem.
+          Sed interdum facilisis ornare.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SideEffects;
