85b00fd05e226b319e01b1f7121eb2a2d43809ab
aalagic <aldin_alagic@hotmail.com>
2020-09-24 21:46:53 +0200

    Create a <ShortDescription> component for displaying information on the solution on view solution page
    Create a <ShortDescription> component  that displays a short description on the solution on view solution page.

---
+++ src/components/findSolution/solutionView/shortDescription.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ShortDescription = () => {
+  return (
+    <div>
+      <h5 className="text-primary bg-light p-2">Short description</h5>
+      <div className="row justify-content-start px-4 mb-2">
+        <p className="text-justify">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec
+          posuere lorem. Sed interdum facilisis ornare. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Fusce nec posuere lorem. Sed
+          interdum facilisis ornare. Sed interdum facilisis ornare. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Fusce nec posuere lorem.
+          Sed interdum facilisis ornare.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ShortDescription;
