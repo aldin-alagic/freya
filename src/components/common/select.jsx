59dbb9a74bc510a224793b4622c62090a9db91e7
aalagic <aldin_alagic@hotmail.com>
2020-09-27 20:31:16 +0200

    Change the <Select> component to use the selectpicker from bootstra-select

--- src/components/common/select.jsx
+++ src/components/common/select.jsx
@@ -4,7 +4,7 @@ const Select = ({ name, label, options, error, ...rest }) => {
   return (
     <div className="form-group row mx-0">
       <label htmlFor={name}>{label}</label>
-      <select name={name} id={name} {...rest} className="form-control">
+      <select name={name} id={name} {...rest} className="selectpicker">
         {options.map((option) => (
           <option key={option.id} value={option.value}>
             {option.text}
