7455919e9b9d9961ce6578b76b9d16787e322498
aalagic <aldin_alagic@hotmail.com>
2020-09-30 20:46:33 +0200

    Create a <ProfileChangePassword> component for displaying a form for changing password

---
+++ src/components/profile/profileChangePassword.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ChangePasswordForm from "./changePasswordForm";
+
+const ProfileChangePassword = () => {
+  return (
+    <div className="col-md-9">
+      <div className="card">
+        <div className="card-body">
+          <ChangePasswordForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileChangePassword;
