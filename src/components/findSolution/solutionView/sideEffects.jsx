19c5e72fe980c9c0724f968fe50253ad45e14dcb
aalagic <aldin_alagic@hotmail.com>
2020-10-26 18:30:13 +0100

    Change text alignment from justify-start to justify in the <SideEffects> component

--- src/components/findSolution/solutionView/sideEffects.jsx
+++ src/components/findSolution/solutionView/sideEffects.jsx
@@ -4,16 +4,14 @@ const SideEffects = () => {
   return (
     <div>
       <h5 className="text-primary bg-light p-2">Side effects</h5>
-      <div className="row justify-content-start px-4 mb-2">
-        <p className="text-justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec
-          posuere lorem. Sed interdum facilisis ornare. Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Fusce nec posuere lorem. Sed
-          interdum facilisis ornare. Sed interdum facilisis ornare. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Fusce nec posuere lorem.
-          Sed interdum facilisis ornare.
-        </p>
-      </div>
+      <p className="text-justify px-3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec
+        posuere lorem. Sed interdum facilisis ornare. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Fusce nec posuere lorem. Sed interdum
+        facilisis ornare. Sed interdum facilisis ornare. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Fusce nec posuere lorem. Sed interdum
+        facilisis ornare.
+      </p>
     </div>
   );
 };
