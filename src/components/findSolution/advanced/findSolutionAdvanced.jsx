fb57ff3934923577450618af1aeb66657a4ff837
aalagic <aldin_alagic@hotmail.com>
2020-10-26 18:29:03 +0100

    Change the design of <FindSolutionAdvanced> component navigation bar

--- src/components/findSolution/advanced/findSolutionAdvanced.jsx
+++ src/components/findSolution/advanced/findSolutionAdvanced.jsx
@@ -10,19 +10,19 @@ const FindSolutionAdvanced = () => {
       <div>
         <div className="mb-4">
           <NavLink
-            className="btn btn-outline-primary col-md-2 m-1"
+            className="btn btn-outline-primary selection-left-option col-md-2"
             to="/find-solution/advanced/vehicle"
           >
             My Vehicle
           </NavLink>
           <NavLink
-            className="btn btn-outline-primary col-md-2 m-1"
+            className="btn btn-outline-primary rounded-0 border-left-0 border-right-0 col-md-2"
             to="/find-solution/advanced/issue-type"
           >
             Issue type
           </NavLink>
           <NavLink
-            className="btn btn-outline-primary col-md-2 m-1"
+            className="btn btn-outline-primary selection-right-option col-md-2"
             to="/find-solution/advanced/additional-filters"
           >
             Additional filters
