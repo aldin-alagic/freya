f97b8233c090155cfbdb170d730e84bbbcd7537b
aalagic <aldin_alagic@hotmail.com>
2020-10-26 18:39:52 +0100

    Change the design of the issues <Menu> component navigation buttons

--- src/components/issues/menu.jsx
+++ src/components/issues/menu.jsx
@@ -1,19 +1,34 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <Fragment>
-      <NavLink className="btn btn-outline-primary col-md-2 m-1" to="/">
-        Resolved
-      </NavLink>
-      <NavLink className="btn btn-outline-primary col-md-2 m-1" to="/home">
-        With bid
-      </NavLink>
-      <NavLink className="btn btn-outline-primary col-md-2 m-1" to="/home">
-        Published
-      </NavLink>
-    </Fragment>
+    <div className="d-flex mb-2">
+      <div className="col-4 p-0">
+        <NavLink
+          className="btn btn-outline-secondary selection-left-option col-12"
+          to="#"
+        >
+          Resolved
+        </NavLink>
+      </div>
+      <div className="col-4 p-0">
+        <NavLink
+          className="btn btn-outline-secondary col-12 rounded-0 border-left-0 border-right-0"
+          to="/home"
+        >
+          With bid
+        </NavLink>
+      </div>
+      <div className="col-4 p-0">
+        <NavLink
+          className="btn btn-outline-secondary selection-right-option col-12"
+          to="/home"
+        >
+          Published
+        </NavLink>
+      </div>
+    </div>
   );
 };
 
