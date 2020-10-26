73b9d323844fabf89a18f2e49c6cfe41ff82cd9b
aalagic <aldin_alagic@hotmail.com>
2020-10-26 18:09:52 +0100

    Move and refine the <ShortDescription> component from findSolution/solutionView to common

---
+++ src/components/common/shortDescription.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ShortDescription = () => {
+  return (
+    <div>
+      <h5 className="text-primary bg-light p-2">Short description</h5>
+      <p className="text-justify px-3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec
+        posuere lorem. Sed interdum facilisis ornare. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Fusce nec posuere lorem. Sed interdum
+        facilisis ornare. Sed interdum facilisis ornare. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Fusce nec posuere lorem. Sed interdum
+        facilisis ornare.
+      </p>
+    </div>
+  );
+};
+
+export default ShortDescription;
