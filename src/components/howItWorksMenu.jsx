aa36e91817315c7fcf674e88590252839ac7a744
aalagic <aldin_alagic@hotmail.com>
2020-10-26 18:21:29 +0100

    Change the design of the <HowItWorksMenu> component navigation buttons

--- src/components/howItWorksMenu.jsx
+++ src/components/howItWorksMenu.jsx
@@ -6,13 +6,13 @@ const HowItWorksMenu = () => {
     <Fragment>
       <h1 className="mb-4">What do you want?</h1>
       <NavLink
-        className="btn btn-outline-primary col-md-2 m-2"
+        className="btn btn-outline-primary selection-left-option col-md-2"
         to="/how-it-works/customer"
       >
         Solution
       </NavLink>
       <NavLink
-        className="btn btn-outline-primary col-md-2 m-2"
+        className="btn btn-outline-primary selection-right-option col-md-2"
         to="/how-it-works/expert"
       >
         Work
