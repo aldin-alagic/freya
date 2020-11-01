4726621d91c2cdb12cef58cbbe2172e839c45b77
aalagic <aldin_alagic@hotmail.com>
2020-11-01 20:35:48 +0100

    Create a <IssueType> component for selecting the type of issue at the new issue page

---
+++ src/components/issues/newIssue/issueType.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import Interior from "./interior";
+import Engine from "./engine";
+import BodyExterior from "./bodyExterior";
+import ControlSuspension from "./controlSuspension";
+import DrivingSupportSecurity from "./drivingSupportSecurity";
+import EnergySystem from "./energySystem";
+import Sensors from "./sensors";
+import AdditionalElectricParts from "./additionalElectricParts";
+import IssueTypeMenu from "./issueTypeMenu";
+
+class IssueType extends Component {
+  state = {
+    issueCategory: null,
+    issueType: null,
+    issueTypeLabel: null,
+  };
+
+  getComponent = () => {
+    const { issueType } = this.props;
+    switch (this.state.issueCategory) {
+      case 1:
+        return <Interior issueType={issueType} onChange={this.handleChange} />;
+      case 2:
+        return <Engine issueType={issueType} onChange={this.handleChange} />;
+      case 3:
+        return (
+          <BodyExterior issueType={issueType} onChange={this.handleChange} />
+        );
+      case 4:
+        return (
+          <ControlSuspension
+            issueType={issueType}
+            onChange={this.handleChange}
+          />
+        );
+      case 5:
+        return (
+          <DrivingSupportSecurity
+            issueType={issueType}
+            onChange={this.handleChange}
+          />
+        );
+      case 6:
+        return (
+          <EnergySystem issueType={issueType} onChange={this.handleChange} />
+        );
+      case 7:
+        return <Sensors issueType={issueType} onChange={this.handleChange} />;
+      case 8:
+        return (
+          <AdditionalElectricParts
+            issueType={issueType}
+            onChange={this.handleChange}
+          />
+        );
+      default:
+        break;
+    }
+  };
+
+  handleChange = ({ target: input }) => {
+    const issueType = input.value;
+    const issueTypeLabel = input.dataset.label;
+    this.props.onIssueTypeChange(issueType, issueTypeLabel);
+    this.setState({ issueType, issueTypeLabel });
+  };
+
+  handleClick = (issueCategory) => {
+    this.setState({ issueCategory });
+  };
+
+  render() {
+    return (
+      <div className="animate__animated animate__fadeIn">
+        <IssueTypeMenu
+          onClick={this.handleClick}
+          active={this.state.issueCategory}
+        />
+        <hr className="mt-3 mb-4" />
+        <div className="mb-3">
+          Selected issue:
+          {this.props.issueTypeLabel != null ? (
+            <span className="text-primary ml-2">
+              {this.props.issueTypeLabel}
+            </span>
+          ) : (
+            <span className="text-danger ml-2">None</span>
+          )}
+        </div>
+        {this.getComponent()}
+      </div>
+    );
+  }
+}
+export default IssueType;
