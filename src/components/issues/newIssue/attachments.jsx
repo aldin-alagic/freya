fc12b73d867937c0cede75647a742ae1a65337cc
aalagic <aldin_alagic@hotmail.com>
2020-11-01 20:40:55 +0100

    Create a <Attachments> component for uploading attachments on the new issue page

---
+++ src/components/issues/newIssue/attachments.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
+
+const Attachments = () => {
+  return (
+    <div className="card mb-4">
+      <div className="card-header bg-light">Attachments</div>
+      <div className="card-body row justify-content-start px-5">
+        <div className="row px-4 mb-2">
+          <FontAwesomeIcon
+            className="text-secondary mx-2"
+            icon={faImage}
+            size="6x"
+          />
+          <FontAwesomeIcon
+            className="text-secondary mx-2"
+            icon={faImage}
+            size="6x"
+          />
+          <FontAwesomeIcon
+            className="text-secondary mx-2"
+            icon={faImage}
+            size="6x"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Attachments;
