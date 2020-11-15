5a403b2a3ef6a56e35cc7c06213e07bcc998753c
aalagic <aldin_alagic@hotmail.com>
2020-11-15 14:15:30 +0100

    Change the <VehicleDescription> component properties and data display
    Change the <VehicleDescription> component so that it uses data property to show information about the vehicle.

--- src/components/common/vehicleDescription.jsx
+++ src/components/common/vehicleDescription.jsx
@@ -1,43 +1,33 @@
 import React from "react";
-import Vehicle from "./../data/vehicle";
 
-const VehicleDescription = () => {
-  const vehicle = new Vehicle(
-    "Mercedes",
-    "C200",
-    "AMG",
-    "2016",
-    "Diesel",
-    "Automatic"
-  );
-
+const VehicleDescription = ({ data }) => {
   return (
     <div>
       <h5 className="text-primary bg-light p-2">Vehicle</h5>
       <div className="row justify-content-between px-4">
         <div className="d-inline-block m-1">
           <span>Brand</span>
-          <h6>{vehicle.brand}</h6>
+          <h6>{data.brand.value}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Model</span>
-          <h6>{vehicle.model}</h6>
+          <h6>{data.model.value}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Version</span>
-          <h6>{vehicle.version}</h6>
+          <h6>{data.version.value}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Year</span>
-          <h6>{vehicle.year}</h6>
+          <h6>{data.year.value}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Fuel type</span>
-          <h6>{vehicle.fuelType}</h6>
+          <h6>{data.fuelType}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Transmission</span>
-          <h6>{vehicle.transmission}</h6>
+          <h6>{data.transmission}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Filter</span>
