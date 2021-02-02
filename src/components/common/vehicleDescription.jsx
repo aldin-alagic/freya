e5203cbce639b5a5d8052757c937db2de0c7e64a
aalagic <aldin_alagic@hotmail.com>
2021-02-02 23:48:39 +0100

    Change variable names of vehicle variables

--- src/components/common/vehicleDescription.jsx
+++ src/components/common/vehicleDescription.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 
-const VehicleDescription = ({ data }) => {
+const VehicleDescription = ({ values }) => {
   return (
     <div>
       <h5 className="text-primary bg-light p-2">Vehicle</h5>
       <div className="row justify-content-between px-4">
         <div className="d-inline-block m-1">
           <span>Brand</span>
-          <h6>{data.brand}</h6>
+          <h6>{values.brand}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Model</span>
-          <h6>{data.model}</h6>
+          <h6>{values.model}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Version</span>
-          <h6>{data.version}</h6>
+          <h6>{values.version}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Year</span>
-          <h6>{data.year}</h6>
+          <h6>{values.year}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Fuel type</span>
-          <h6>{data.fuelType}</h6>
+          <h6>{values.fuelTypeOption}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Transmission</span>
-          <h6>{data.transmission}</h6>
+          <h6>{values.transmission}</h6>
         </div>
         <div className="d-inline-block m-1">
           <span>Filter</span>
