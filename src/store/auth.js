0abdf460f7a4da831b00bc924f089a64a96b84c6
aalagic <aldin_alagic@hotmail.com>
2021-02-10 01:56:29 +0100

    Create a Redux Duck file for authentication
    Create Redux Duck file auth.js which holds the Slice with reducers, action creators and selectors for authentication.

---
+++ src/store/auth.js
@@ -0,0 +1,76 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+import { apiCallBegan } from "./api";
+
+import { AUTH_TOKEN, AUTH_URL } from "../config.json";
+
+const slice = createSlice({
+  name: "auth",
+  initialState: {
+    token: localStorage.getItem(AUTH_TOKEN),
+    user: {},
+    loading: false,
+    apiResult: {
+      status: 0,
+      message: "",
+    },
+  },
+
+  reducers: {
+    authenticationStarted: (auth, action) => {
+      auth.loading = true;
+    },
+
+    authenticated: (auth, action) => {
+      const { data, status, message } = action.payload;
+
+      if (status === 200) {
+        auth.token = data.auth_token;
+        auth.user = {
+          email: data.email,
+          username: data.username,
+          firstname: data.firstname,
+          lastname: data.lastname,
+        };
+        auth.loading = false;
+        auth.apiResult = {
+          status,
+          message,
+        };
+        toast.success(message, { className: "alert-success" });
+      } else toast.error(message, { className: "alert-danger" });
+      auth.loading = false;
+    },
+
+    deAuthenticated: (auth, action) => {
+      auth.token = "";
+      toast.success("You have been succesfuly logged out", {
+        className: "alert-success",
+      });
+    },
+
+    authenticationFailed: (auth, action) => {
+      auth.loading = false;
+    },
+  },
+});
+
+export const {
+  authenticationStarted,
+  authenticated,
+  deAuthenticated,
+  authenticationFailed,
+} = slice.actions;
+
+export default slice.reducer;
+
+export const authenticateUser = (email, password) =>
+  apiCallBegan({
+    url: AUTH_URL,
+    method: "POST",
+    data: { email, password },
+    onStart: authenticationStarted.type,
+    onSuccess: authenticated.type,
+    onError: authenticationFailed.type,
+  });
