e9531e2c4b17503d1cfabb7cd96c690013e01bc5
aalagic <aldin_alagic@hotmail.com>
2021-02-10 02:06:24 +0100

    Connect <Logout> component to the Redux store so that the process of logging out the user is done via store

--- src/components/logout/Logout.jsx
+++ src/components/logout/Logout.jsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { connect } from "react-redux";
 
-import auth from "../../services/userService";
+import { deAuthenticated } from "./../../store/auth";
 
-export class Logout extends React.PureComponent {
+import { AUTH_TOKEN } from "../../config.json";
+
+class Logout extends React.PureComponent {
   componentDidMount() {
-    auth.logout();
-    window.location = "/";
+    const { location, history, deAuthenticateUser } = this.props;
+    localStorage.removeItem(AUTH_TOKEN);
+    deAuthenticateUser();
+    history.push(location.state ? location.state.from.pathname : "/");
   }
 
   render() {
     return null;
   }
 }
+
+const mapStateToProps = (state) => ({
+  loading: state.entities.auth.loading,
+  token: state.entities.auth.token,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  deAuthenticateUser: () => dispatch(deAuthenticated()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Logout);
