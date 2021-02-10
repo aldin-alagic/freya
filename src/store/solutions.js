53ee34343400f056ae0b1b43a86f4826eedd73f2
aalagic <aldin_alagic@hotmail.com>
2021-02-10 03:11:48 +0100

    Create a Redux Duck file for Solutions
    Create Redux Duck file solutions.js which holds the Slice with reducers, action creators and selectors for solutions.

---
+++ src/store/solutions.js
@@ -0,0 +1,70 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import moment from "moment";
+
+import { apiCallBegan } from "./api";
+
+import { ERROR_MESSAGE, CACHE_PERIOD } from "../config.json";
+
+const slice = createSlice({
+  name: "solutions",
+  initialState: {
+    list: [],
+    apiResult: {
+      status: 0,
+      message: "",
+    },
+    loading: false,
+    lastFetch: null,
+  },
+  reducers: {
+    solutionsRequested: (solutions, action) => {
+      solutions.loading = true;
+    },
+
+    solutionsReceived: (solutions, action) => {
+      const { data, status, message } = action.payload;
+
+      if (status === 200) {
+        solutions.apiResult = {
+          status,
+          message,
+        };
+        toast.success(message, { className: "alert-success" });
+      } else toast.error(message, { className: "alert-danger" });
+      solutions.loading = false;
+    },
+
+    solutionsRequestFailed: (solutions, action) => {
+      solutions.loading = false;
+      toast.error(ERROR_MESSAGE, { className: "alert-danger" });
+    },
+  },
+});
+
+export const {
+  solutionsRequested,
+  solutionsReceived,
+  solutionsRequestFailed,
+} = slice.actions;
+
+export default slice.reducer;
+
+export const loadSolutions = () => (dispatch, getState) => {
+  const { lastFetch } = getState().entities.solutions;
+  const { token } = getState().auth;
+
+  const diffInMinutes = moment().diff(moment(lastFetch), "minutes");
+  if (diffInMinutes < CACHE_PERIOD) return;
+
+  return dispatch(
+    apiCallBegan({
+      url: "/get-solutions",
+      method: "GET",
+      headers: { Auth_Token: token },
+      onStart: solutionsRequested.type,
+      onSuccess: solutionsReceived.type,
+      onError: solutionsRequestFailed.type,
+    })
+  );
+};
