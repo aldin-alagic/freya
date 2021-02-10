44207299e79f9eb09d7920bfb58ab7ce2623107f
aalagic <aldin_alagic@hotmail.com>
2021-02-10 02:11:00 +0100

    Fix imports so they match new imports of Spinner, Navbar, Login, Logout, ProtectedRoute components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import ReactTooltip from "react-tooltip";
 
-import { Spinner } from "./components/spinner/Spinner";
-import { NavBar } from "./components/navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import { Footer } from "./components/footer/Footer";
 import { HelpAndSupport } from "./components/helpAndSupport/HelpAndSupport";
 import { Faq } from "./components/faq/Faq";
@@ -23,9 +22,9 @@ import { Profile } from "./components/profile/Profile";
 import { Issues } from "./components/issues/Issues";
 import { Issue } from "./components/issues/issue/Issue";
 import { NewIssue } from "./components/issues/newIssue/NewIssue";
-import { Login } from "./components/login/Login";
-import { Logout } from "./components/logout/Logout";
-import { ProtectedRoute } from "./components/common/protectedRoute/ProtectedRoute";
+import Login from "./components/login/Login";
+import Logout from "./components/logout/Logout";
+import ProtectedRoute from "./components/common/protectedRoute/ProtectedRoute";
 import { Home } from "./components/home/Home";
 import auth from "./services/userService";
 import { Offer } from "./components/offer/Offer";
@@ -46,50 +45,52 @@ class App extends Component {
     const { user } = this.state;
 
     return (
-      <div className="main-container">
-        <ReactTooltip place="right" type="dark" effect="solid" />
-        <Spinner />
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          bodyClassName="p-3 text-justify"
-        />
-        <Report />
-        <NavBar user={user} />
-        <main className="container col-9">
-          <Switch>
-            <Route path="/help-and-support" component={HelpAndSupport} />
-            <Route path="/faq" component={Faq} />
-            <ProtectedRoute path="/submit-ticket" component={SubmitTicket} />
-            <Route path="/contact-us" component={ContactUs} />
-            <Route path="/user-agreement" component={UserAgreement} />
-            <Route path="/privacy-policy" component={PrivacyPolicy} />
-            <Route path="/about-us" component={AboutUs} />
-            <Route path="/pricing" component={Pricing} />
-            <Route path="/how-it-works" component={HowItWorks} />
-            <Route path="/become-an-expert" component={BecomeExpert} />
-            <Route path="/solutions" component={Solutions} />
-            <Route path="/solution/test" component={Solution} />
-            <Route path="/issues/test/" component={Issue} />
-            <Route path="/issues/new/" component={NewIssue} />
-            <Route path="/issues" component={Issues} />
-            <Route path="/offers/:id" component={Offer} />
-            <ProtectedRoute path="/profile" component={Profile} />
-            <Route path="/login" component={Login} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" component={Home} />
-            <Redirect to="/" />
-          </Switch>
-        </main>
-        <Footer />
-      </div>
+      <Provider store={this.store}>
+        <div className="main-container">
+          <ReactTooltip place="right" type="dark" effect="solid" />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            limit={1}
+            draggable
+            pauseOnHover
+            bodyClassName="p-3 text-justify"
+          />
+          <Report />
+          <Navbar />
+          <main className="container col-9">
+            <Switch>
+              <Route path="/help-and-support" component={HelpAndSupport} />
+              <Route path="/faq" component={Faq} />
+              <ProtectedRoute path="/submit-ticket" component={SubmitTicket} />
+              <Route path="/contact-us" component={ContactUs} />
+              <Route path="/user-agreement" component={UserAgreement} />
+              <Route path="/privacy-policy" component={PrivacyPolicy} />
+              <Route path="/about-us" component={AboutUs} />
+              <Route path="/pricing" component={Pricing} />
+              <Route path="/how-it-works" component={HowItWorks} />
+              <Route path="/become-an-expert" component={BecomeExpert} />
+              <Route path="/solutions" component={Solutions} />
+              <Route path="/solution/test" component={Solution} />
+              <Route path="/issues/test/" component={Issue} />
+              <Route path="/issues/new/" component={NewIssue} />
+              <Route path="/issues" component={Issues} />
+              <Route path="/offers/:id" component={Offer} />
+              <ProtectedRoute path="/profile" component={Profile} />
+              <Route path="/login" component={Login} />
+              <Route path="/logout" component={Logout} />
+              <Route path="/" component={Home} />
+              <Redirect to="/" />
+            </Switch>
+          </main>
+          <Footer />
+        </div>
+      </Provider>
     );
   }
 }
