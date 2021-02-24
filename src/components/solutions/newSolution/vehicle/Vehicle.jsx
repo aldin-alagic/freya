8efce63dada6a08c78d297e12f462cecebdb758f
aalagic <aldin_alagic@hotmail.com>
2021-02-24 02:11:57 +0100

    Change Vehicle component so that it used redux store and react-hook-form

--- src/components/solutions/newSolution/vehicle/Vehicle.jsx
+++ src/components/solutions/newSolution/vehicle/Vehicle.jsx
@@ -1,228 +1,327 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
 import makeAnimated from "react-select/animated";
 import Select from "react-select";
-import { PropTypes } from "prop-types";
 
-import { Radio } from "../../../common/form/radio/Radio";
+import { newSolutionUpdated } from "../../../../store/solutions";
+import StepNavigator from "../StepNavigator/StepNavigator";
+import { Options } from "./../options/Options";
 
-export class Vehicles extends React.PureComponent {
-  static propTypes = {
-    values: PropTypes.object.isRequired,
-    onChange: PropTypes.func.isRequired,
-    onSelectChange: PropTypes.func.isRequired,
-    onChangeVehicles: PropTypes.func.isRequired,
+import {
+  transmissionOptions,
+  fuelTypeOptions,
+} from "../../../../utils/staticData";
+import { FORM_REQUIRED_MESSAGE } from "../../../../config.json";
+
+export function Vehicle() {
+  const animatedComponents = makeAnimated();
+  const dispatch = useDispatch();
+
+  const transmission = useSelector(
+    (state) => state.entities.solutions.newSolution.transmission
+  );
+  const fuelType = useSelector(
+    (state) => state.entities.solutions.newSolution.fuelType
+  );
+  const { brands, models, variants, years } = useSelector(
+    (state) => state.vehicles
+  );
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    getValues,
+    formState,
+    control,
+    errors,
+  } = useForm({ defaultValues: { transmission, fuelType } });
+
+  const [vehicles, setVehicles] = useState([
+    ...useSelector((state) => state.entities.solutions.newSolution.vehicles),
+  ]);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    let newVehicles = [...vehicles];
+
+    for (let i = 0; i < newVehicles.length; i++) {
+      let years = data[`year-${i}`].map((year) => year.value);
+      newVehicles[i] = {
+        brand: data[`brand-${i}`].value,
+        model: data[`model-${i}`].value,
+        variant: data[`variant-${i}`] ? data[`variant-${i}`].value : null,
+        years,
+      };
+    }
+
+    dispatch(
+      newSolutionUpdated({
+        status: "process",
+        step: 1,
+        vehicles: newVehicles,
+        transmission: data.transmission,
+        fuelType: data.fuelType,
+      })
+    );
   };
 
-  render() {
-    const { values, onChangeVehicles, onChange, onSelectChange } = this.props;
-    const animatedComponents = makeAnimated();
-    const transmitionOptions = ["Manual", "Semi-automatic", "Automatic"];
-    const fuelTypeOptions = [
-      "Petrol",
-      "Diesel",
-      "Electric",
-      "Ethanol (FFV, E85, etc.)",
-      "Hybrid (Petrol/Electric)",
-      "Hybrid (Diesel/Electric)",
-      "Hydrogen",
-      "Natural gas",
-      "Other",
-    ];
+  const handleVehiclesChange = (action) => {
+    let newVehicles = [...vehicles];
+    if (action === "ADD") {
+      newVehicles.push({
+        brand: null,
+        model: null,
+        year: null,
+        variant: null,
+      });
+      setVehicles([...newVehicles]);
+    } else if (action === "REMOVE" && vehicles.length > 1) {
+      newVehicles.pop();
+      setVehicles([...newVehicles]);
+    }
+  };
 
-    return (
-      <div className="animate__animated animate__fadeIn">
-        <div className="card mb-4">
-          <div className="card-header bg-light">Brand, Model, Version</div>
-          <div className="card-body">
-            {values.vehicles.map((val, index) => {
-              let brandId = `brand-${index}`,
-                modelId = `model-${index}`,
-                yearId = `year-${index}`,
-                variantId = `variant-${index}`;
+  !(Object.keys(formState.errors).length === 0) &&
+    dispatch(
+      newSolutionUpdated({
+        status: "error",
+      })
+    );
 
-              return (
-                <div
-                  className="row justify-content-center animate__animated animate__fadeIn my-3"
-                  key={index}
-                >
-                  <div className="col-md-3">
-                    <Select
-                      name={brandId}
-                      placeholder="Brand"
-                      label="Brand"
-                      options={[
-                        { value: "Mercedes", label: "Mercedes" },
-                        { value: "BMW", label: "BMW" },
-                        { value: "Audi", label: "Audi" },
-                      ]}
-                      value={{
-                        value:
-                          values.vehicles[index].brand !== null
-                            ? values.vehicles[index].brand
-                            : "Brand",
-                        label:
-                          values.vehicles[index].brand !== null
-                            ? values.vehicles[index].brand
-                            : "Brand",
-                      }}
-                      menuPortalTarget={document.body}
-                      styles={{
-                        menuPortal: (base) => ({ ...base, zIndex: 9999 }),
-                      }}
-                      isSearchable
-                      isClearable
-                      onChange={onSelectChange}
-                      components={animatedComponents}
-                    />
+  const showFuelTypeOptions = fuelTypeOptions.map((option, index) => (
+    <div key={index} className="col-sm-6 col-md-3">
+      <div key={index} className="form-check m-2">
+        <input
+          type="radio"
+          id={"fuelType-" + option}
+          name="fuelType"
+          className="form-check-input"
+          value={option}
+          ref={register({
+            required: FORM_REQUIRED_MESSAGE,
+          })}
+        />
+        <label className="form-check-label" htmlFor="fuelType">
+          {option}
+        </label>
+      </div>
+    </div>
+  ));
+
+  const showTransmissionOptions = transmissionOptions.map((option, index) => (
+    <div key={index} className="col-sm-6 col-md-3">
+      <div key={index} className="form-check m-2">
+        <input
+          type="radio"
+          id={"transmission-" + option}
+          name="transmission"
+          className="form-check-input"
+          value={option}
+          ref={register({
+            required: FORM_REQUIRED_MESSAGE,
+          })}
+        />
+        <label className="form-check-label" htmlFor="transmission">
+          {option}
+        </label>
+      </div>
+    </div>
+  ));
+
+  return (
+    <form
+      className="animate__animated animate__fadeIn"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="card mb-4">
+        <div className="card-header bg-light">Brand, Model, Variant</div>
+        <div className="card-body pb-3">
+          {vehicles.map((vehicle, index) => {
+            let brandId = `brand-${index}`,
+              modelId = `model-${index}`,
+              yearId = `year-${index}`,
+              variantId = `variant-${index}`;
+
+            return (
+              <div
+                className="row animate__animated animate__fadeIn my-3"
+                key={index}
+              >
+                <div className="col-md-3">
+                  <Controller
+                    control={control}
+                    rules={{ required: FORM_REQUIRED_MESSAGE }}
+                    name={brandId}
+                    placeholder="Brand"
+                    label="Brand"
+                    options={brands}
+                    defaultValue={
+                      vehicle.brand !== null && {
+                        value: vehicle.brand,
+                        label: vehicle.brand,
+                      }
+                    }
+                    menuPortalTarget={document.body}
+                    styles={{
+                      menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+                    }}
+                    isSearchable
+                    isClearable
+                    components={animatedComponents}
+                    as={Select}
+                  />
+                  <div className="mt-2 text-danger">
+                    {errors[brandId]?.message}
                   </div>
+                </div>
 
+                {(watch(brandId) || vehicle.brand) && (
                   <div className="col-md-3">
-                    <Select
+                    <Controller
+                      control={control}
+                      rules={{ required: FORM_REQUIRED_MESSAGE }}
                       name={modelId}
                       placeholder="Model"
                       label="Model"
-                      options={[
-                        { value: "C200", label: "C200" },
-                        { value: "E220", label: "E220" },
-                        { value: "S230", label: "S230" },
-                      ]}
-                      value={{
-                        value:
-                          values.vehicles[index].model !== null
-                            ? values.vehicles[index].model
-                            : "Model",
-                        label:
-                          values.vehicles[index].model !== null
-                            ? values.vehicles[index].model
-                            : "Model",
-                      }}
+                      options={
+                        watch(brandId) && models[getValues(brandId).value]
+                      }
+                      defaultValue={
+                        vehicle.model !== null && {
+                          value: vehicle.model,
+                          label: vehicle.model,
+                        }
+                      }
                       menuPortalTarget={document.body}
                       styles={{
                         menuPortal: (base) => ({ ...base, zIndex: 9999 }),
                       }}
                       isSearchable
                       isClearable
-                      onChange={onSelectChange}
                       components={animatedComponents}
+                      as={Select}
                     />
+                    <div className="mt-2 text-danger">
+                      {errors[modelId]?.message}
+                    </div>
                   </div>
+                )}
 
-                  <div className="col-md-3">
-                    <Select
-                      name={yearId}
-                      placeholder="Year"
-                      label="Year"
-                      options={[
-                        { value: "2010", label: "2010" },
-                        { value: "2011", label: "2011" },
-                        { value: "2012", label: "2012" },
-                      ]}
-                      value={{
-                        value:
-                          values.vehicles[index].year !== null
-                            ? values.vehicles[index].year
-                            : "Year",
-                        label:
-                          values.vehicles[index].year !== null
-                            ? values.vehicles[index].year
-                            : "Year",
-                      }}
-                      menuPortalTarget={document.body}
-                      styles={{
-                        menuPortal: (base) => ({ ...base, zIndex: 9999 }),
-                      }}
-                      isSearchable
-                      isClearable
-                      onChange={onSelectChange}
-                      components={animatedComponents}
-                    />
-                  </div>
-
-                  <div className="col-md-3">
-                    <Select
-                      name={variantId}
-                      placeholder="Variant"
-                      label="Variant"
-                      options={[
-                        { value: "AMG", label: "AMG" },
-                        { value: "GTR", label: "GTR" },
-                      ]}
-                      value={{
-                        value:
-                          values.vehicles[index].variant !== null
-                            ? values.vehicles[index].variant
-                            : "Variant",
-                        label:
-                          values.vehicles[index].variant !== null
-                            ? values.vehicles[index].variant
-                            : "Variant",
-                      }}
-                      menuPortalTarget={document.body}
-                      styles={{
-                        menuPortal: (base) => ({ ...base, zIndex: 9999 }),
-                      }}
-                      isSearchable
-                      isClearable
-                      onChange={onSelectChange}
-                      components={animatedComponents}
-                    />
-                  </div>
-                </div>
-              );
-            })}
+                {(watch(modelId) || vehicle.model) && (
+                  <React.Fragment>
+                    <div className="col-md-3">
+                      <Controller
+                        control={control}
+                        rules={{ required: FORM_REQUIRED_MESSAGE }}
+                        name={yearId}
+                        placeholder="Years"
+                        label="Years"
+                        options={years}
+                        defaultValue={
+                          vehicle.years.length > 0
+                            ? vehicle.years.map((year) => {
+                                return { value: year, label: year };
+                              })
+                            : null
+                        }
+                        menuPortalTarget={document.body}
+                        isMulti
+                        styles={{
+                          menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+                        }}
+                        isSearchable
+                        isClearable
+                        components={animatedComponents}
+                        as={Select}
+                      />
+                      <div className="text-danger">
+                        {errors[yearId]?.message}
+                      </div>
+                    </div>
+                    <div className="col-md-3">
+                      <Controller
+                        control={control}
+                        name={variantId}
+                        placeholder="Variant"
+                        label="Variant"
+                        options={
+                          watch(modelId) && variants[getValues(modelId).value]
+                        }
+                        defaultValue={
+                          vehicle.variant !== null && {
+                            value: vehicle.variant,
+                            label: vehicle.variant,
+                          }
+                        }
+                        menuPortalTarget={document.body}
+                        styles={{
+                          menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+                        }}
+                        isSearchable
+                        isClearable
+                        components={animatedComponents}
+                        as={Select}
+                      />
+                      <div className="text-danger">
+                        {errors[variantId]?.message}
+                      </div>
+                    </div>
+                  </React.Fragment>
+                )}
+              </div>
+            );
+          })}
+          <button
+            type="button"
+            className="text-primary bg-white mb-2 mr-3"
+            onClick={() => handleVehiclesChange("ADD")}
+          >
+            Add another vehicle
+          </button>
+          {vehicles.length > 1 && (
             <button
-              className="text-primary bg-white my-3 mr-3"
-              onClick={() => onChangeVehicles("add")}
+              type="button"
+              className="text-danger bg-white mb-2"
+              onClick={() => handleVehiclesChange("REMOVE")}
             >
-              Add another vehicle
+              Remove vehicle
             </button>
-            {values.vehicles.length > 1 ? (
-              <button
-                className="text-danger bg-white my-3"
-                onClick={() => onChangeVehicles("remove")}
-              >
-                Remove vehicle
-              </button>
-            ) : (
-              ""
-            )}
-          </div>
-        </div>
-
-        <div className="card mb-4">
-          <div className="card-header bg-light">Fuel type</div>
-          <div className="card-body row justify-content-start px-5">
-            {fuelTypeOptions.map((option) => (
-              <Radio
-                key={"fuel-type-" + option}
-                id={"fuelType-" + option}
-                name="fuelType"
-                label={option}
-                value={option}
-                checked={values.fuelType == option ? true : false}
-                onChange={onChange}
-              />
-            ))}
-          </div>
-        </div>
-
-        <div className="card mb-4">
-          <div className="card-header bg-light">Transmission</div>
-          <div className="card-body row justify-content-around px-5">
-            {transmitionOptions.map((option) => (
-              <Radio
-                key={"transmission-type-" + option}
-                id={"transmission-" + option}
-                name="transmission"
-                label={option}
-                value={option}
-                checked={values.transmission == option ? true : false}
-                onChange={onChange}
-              />
-            ))}
-          </div>
+          )}
         </div>
       </div>
-    );
-  }
+
+      <div className="card mb-4">
+        <div className="card-header bg-light">Fuel type</div>
+        <div className="card-body row justify-content-start px-5 pb-2">
+          <Options
+            type="fuelType"
+            options={fuelTypeOptions}
+            register={register({
+              required: FORM_REQUIRED_MESSAGE,
+            })}
+          />
+        </div>
+        <div className="ml-5 mb-2 text-danger">{errors.fuelType?.message}</div>
+      </div>
+
+      <div className="card mb-4">
+        <div className="card-header bg-light">Transmission</div>
+        <div className="card-body row justify-content-around px-5 pb-2">
+          <Options
+            type="transmission"
+            options={transmissionOptions}
+            register={register({
+              required: FORM_REQUIRED_MESSAGE,
+            })}
+          />
+        </div>
+        <div className="ml-5 mb-2 text-danger">
+          {errors.transmission?.message}
+        </div>
+      </div>
+      <StepNavigator currentStep={0} onNextStepClick={handleSubmit(onSubmit)} />
+    </form>
+  );
 }
