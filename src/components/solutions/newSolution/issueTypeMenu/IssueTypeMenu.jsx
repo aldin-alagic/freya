55be0ebb87a5d0a358a2fb54a7e89ec303e18b76
aalagic <aldin_alagic@hotmail.com>
2021-02-24 02:13:26 +0100

    Change the IssueType and IssueTypeMenu  components so that they use redux store and react-hook-form

--- src/components/solutions/newSolution/issueTypeMenu/IssueTypeMenu.jsx
+++ src/components/solutions/newSolution/issueTypeMenu/IssueTypeMenu.jsx
@@ -1,41 +1,35 @@
 import React from "react";
 import { PropTypes } from "prop-types";
 
+import { issueTypes } from "./../../../../utils/staticData";
+
 export class IssueTypeMenu extends React.PureComponent {
   static propTypes = {
     active: PropTypes.number.isRequired,
     onClick: PropTypes.func.isRequired,
+    unregister: PropTypes.func.isRequired,
   };
 
   render() {
-    const { onClick, active } = this.props;
+    const { onClick, unregister, active } = this.props;
+
     const activeStyle = "btn btn-sm btn-primary col-12";
     const inactiveStyle = "btn btn-sm btn-outline-primary col-12";
-    const issueTypes = [
-      "Additional electric parts",
-      "Body and exterior",
-      "Control and suspension",
-      "Driving support and security",
-      "Engine",
-      "Interior",
-      "Sensors",
-    ];
-
-    return (
-      <div className="row mx-0">
-        {issueTypes.map((val, index) => {
-          return (
-            <div className="col-lg-3 col-md-4 col-sm-6 p-1">
-              <button
-                className={active === index ? activeStyle : inactiveStyle}
-                onClick={() => onClick(index)}
-              >
-                {val}
-              </button>
-            </div>
-          );
-        })}
+    const issueTypeCategories = issueTypes.map((val, index) => (
+      <div key={index} className="col-lg-3 col-md-4 col-sm-6 p-1">
+        <button
+          type="button"
+          className={active === index ? activeStyle : inactiveStyle}
+          onClick={() => {
+            unregister("issueTypeOption");
+            onClick(index);
+          }}
+        >
+          {val.title}
+        </button>
       </div>
-    );
+    ));
+
+    return <div className="row mx-0">{issueTypeCategories}</div>;
   }
 }
