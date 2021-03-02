a80dfdf3bc5a5e2c048a61bc2b8ae495f776b087
aalagic <aldin_alagic@hotmail.com>
2021-03-02 21:04:35 +0100

    Change Widgets import in Solutions Basic component

--- src/components/solutions/basic/Basic.jsx
+++ src/components/solutions/basic/Basic.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Sidebar } from "./sidebar/Sidebar";
-import { Widgets } from "./widgets/Widgets";
+import Widgets from "./widgets/Widgets";
 import Main from "./main/Main";
 
 export class Basic extends React.PureComponent {
