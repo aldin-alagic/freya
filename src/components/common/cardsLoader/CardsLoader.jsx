ac08f33efa8c2d2409dbf4a34a831b81e7110211
aalagic <aldin_alagic@hotmail.com>
2021-03-02 19:44:59 +0100

    Change solution/issue card loader so that it has 3 loading cards instead of 2

--- src/components/common/cardsLoader/CardsLoader.jsx
+++ src/components/common/cardsLoader/CardsLoader.jsx
@@ -8,7 +8,7 @@ export class CardsLoader extends React.PureComponent {
       <ContentLoader
         speed={2}
         className="w-100"
-        height={450}
+        height={650}
         backgroundColor="#f3f3f3"
         foregroundColor="#ecebeb"
       >
@@ -29,6 +29,15 @@ export class CardsLoader extends React.PureComponent {
         <rect x="3" y="332" rx="3" ry="3" className="w-100" height="20" />
         <rect x="3" y="362" rx="3" ry="3" className="w-100" height="20" />
         <rect x="3" y="392" rx="3" ry="3" className="w-100" height="20" />
+
+        <rect x="2" y="432" rx="0" ry="0" width="100" height="71" />
+        <rect x="120" y="432" rx="3" ry="3" width="90" height="10" />
+        <rect x="120" y="454" rx="3" ry="3" width="90" height="10" />
+        <rect x="120" y="476" rx="3" ry="3" width="52" height="10" />
+        <rect x="3" y="514" rx="3" ry="3" className="w-100" height="20" />
+        <rect x="3" y="544" rx="3" ry="3" className="w-100" height="20" />
+        <rect x="3" y="574" rx="3" ry="3" className="w-100" height="20" />
+        <rect x="3" y="604" rx="3" ry="3" className="w-100" height="20" />
       </ContentLoader>
     );
   }
