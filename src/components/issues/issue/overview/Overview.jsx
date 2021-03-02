3c830a0150830fcb87f95c9537b306195118a690
aalagic <aldin_alagic@hotmail.com>
2021-03-02 20:33:29 +0100

    Remove VehicleDescription component import from Overview

--- src/components/issues/issue/overview/Overview.jsx
+++ src/components/issues/issue/overview/Overview.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import { Attachments } from "../../../common/attachments/Attachments";
 import { Offers } from "../offers/Offers";
-import { VehicleDescription } from "../../../common/vehicleDescription/VehicleDescription";
 import { IssueType } from "../../../common/issueType/IssueType";
 import { ShortDescription } from "../../../common/shortDescription/ShortDescription";
 import { Activities } from "../activities/Activities";
@@ -12,16 +11,6 @@ export class Overview extends React.PureComponent {
   render() {
     return (
       <React.Fragment>
-        <VehicleDescription
-          data={{
-            brand: "Mercedes",
-            model: "C200",
-            year: "2019",
-            version: "AMG",
-            fuelType: "Hybrid",
-            transmission: "Manual",
-          }}
-        />
         <IssueType />
         <ShortDescription />
         <Attachments />
