aea675b2a8574907cd767edb12a78fb8bdb6ce87
aalagic <aldin_alagic@hotmail.com>
2021-03-02 19:48:32 +0100

    Add files reducer to the root reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 
 import authReducer from "./auth";
+import filesReducer from "./files";
 import vehiclesReducer from "./vehicles";
 import entitiesReducer from "./entities";
 
 export default combineReducers({
   auth: authReducer,
+  files: filesReducer,
   entities: entitiesReducer,
   vehicles: vehiclesReducer,
 });
