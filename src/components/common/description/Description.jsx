16841aa5f8a408f05445e741ff26c4eb84e69666
aalagic <aldin_alagic@hotmail.com>
2021-03-02 21:01:59 +0100

    Change Description component so that it uses DescriptionText, ItemList, VehicleDescriptions component

--- src/components/common/description/Description.jsx
+++ src/components/common/description/Description.jsx
@@ -1,16 +1,61 @@
 import React from "react";
+import { PropTypes } from "prop-types";
 
 import { IssueType } from "../issueType/IssueType";
-import { ShortDescription } from "../shortDescription/ShortDescription";
-import { VehicleDescription } from "../vehicleDescription/VehicleDescription";
+import { DescriptionText } from "./../descriptionText/DescriptionText";
+import { ItemList } from "../itemList/ItemList";
+import { VehicleDescriptions } from "./../vehicleDescriptions/VehicleDescriptions";
 
 export class Description extends React.PureComponent {
+  static propTypes = {
+    vehicles: PropTypes.arrayOf(PropTypes.object).isRequired,
+    fuelType: PropTypes.string.isRequired,
+    transmission: PropTypes.string.isRequired,
+    issueTypeOption: PropTypes.string.isRequired,
+    shortDescription: PropTypes.string.isRequired,
+    longDescription: PropTypes.string,
+    parts: PropTypes.arrayOf(PropTypes.string).isRequired,
+    tools: PropTypes.arrayOf(PropTypes.string).isRequired,
+    keywords: PropTypes.arrayOf(PropTypes.string).isRequired,
+    owner: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    longDescription: null,
+  };
+
   render() {
+    const {
+      vehicles,
+      fuelType,
+      transmission,
+      issueTypeOption,
+      shortDescription,
+      longDescription,
+      parts,
+      tools,
+      keywords,
+      owner,
+    } = this.props;
+
     return (
       <React.Fragment>
-        <VehicleDescription />
-        <IssueType />
-        <ShortDescription />
+        <VehicleDescriptions
+          vehicles={vehicles}
+          fuelType={fuelType}
+          transmission={transmission}
+        />
+        <IssueType option={issueTypeOption} />
+        <DescriptionText title="Short description" text={shortDescription} />
+        {owner && (
+          <DescriptionText
+            title="Detailed description"
+            text={longDescription}
+          />
+        )}
+        <ItemList title="Parts" items={parts} />
+        <ItemList title="Tools" items={tools} />
+        <ItemList title="Keywords" items={keywords} />
       </React.Fragment>
     );
   }
