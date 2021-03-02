6889107d2f622fcdcf84fba4e12a39c64644f9dd
aalagic <aldin_alagic@hotmail.com>
2021-03-02 20:06:03 +0100

    Change ShortDescription component so that it displays text from props instead hardcoded values

--- src/components/common/shortDescription/ShortDescription.jsx
+++ src/components/common/shortDescription/ShortDescription.jsx
@@ -1,18 +1,16 @@
 import React from "react";
+import { PropTypes } from "prop-types";
 
 export class ShortDescription extends React.PureComponent {
+  static propTypes = {
+    description: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <div>
         <h5 className="text-primary bg-light p-2">Short description</h5>
-        <p className="text-justify px-3">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec
-          posuere lorem. Sed interdum facilisis ornare. Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Fusce nec posuere lorem. Sed
-          interdum facilisis ornare. Sed interdum facilisis ornare. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Fusce nec posuere lorem.
-          Sed interdum facilisis ornare.
-        </p>
+        <p className="text-justify my-3 px-3">{this.props.description}</p>
       </div>
     );
   }
