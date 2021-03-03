0bd00459a565cf628b8c2b01308235feebfd0bb5
aalagic <aldin_alagic@hotmail.com>
2021-03-03 22:46:14 +0100

    Remove like and dislikes from Solution component menu

--- src/components/solutions/solution/menu/Menu.jsx
+++ src/components/solutions/solution/menu/Menu.jsx
@@ -5,14 +5,7 @@ import { NavLink } from "react-router-dom";
 import { PropTypes } from "prop-types";
 
 export class Menu extends React.PureComponent {
-  static propTypes = {
-    likes: PropTypes.number.isRequired,
-    dislikes: PropTypes.number.isRequired,
-  };
-
   render() {
-    const { likes, dislikes } = this.props;
-
     return (
       <div className="navbar navbar-expand-lg navbar-light px-0">
         <div className="navbar-nav mr-auto">
@@ -28,14 +21,6 @@ export class Menu extends React.PureComponent {
           <NavLink className="nav-item nav-link" to="expert">
             Expert
           </NavLink>
-        </div>
-        <div>
-          <span className="m-1 text-primary">
-            {likes} <FontAwesomeIcon icon={faThumbsUp} size="sm" />
-          </span>
-          <span className="m-1 text-danger">
-            {dislikes} <FontAwesomeIcon icon={faThumbsDown} size="sm" />
-          </span>
         </div>
       </div>
     );
