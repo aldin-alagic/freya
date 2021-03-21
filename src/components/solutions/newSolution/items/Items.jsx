9c24b13c30532180f33c3adf04a7ef5c8e1dac0d
aldin-alagic <aldin@dysordian.com>
2021-03-21 11:55:49 +0100

    Move items component and style

--- src/components/solutions/newSolution/items/Items.jsx
+++ src/components/solutions/newSolution/items/Items.jsx
@@ -3,24 +3,30 @@ import { PropTypes } from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { CSSTransitionGroup } from "react-transition-group";
-import { InputGroup } from "./../../../../common/formNew/inputGroup/InputGroup";
+import { InputGroup } from "../../../common/formNew/inputGroup/InputGroup";
 
 import "./Items.css";
 
 export class Items extends React.PureComponent {
   static propTypes = {
     name: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    placeholder: PropTypes.string,
     items: PropTypes.arrayOf(PropTypes.string).isRequired,
     register: PropTypes.func.isRequired,
     onItemsClick: PropTypes.func.isRequired,
   };
 
+  static defaultProps = {
+    label: "",
+    placeholder: "",
+  };
+
   render() {
-    const { name, label, items, register, onItemsClick } = this.props;
+    const { name, label, placeholder, items, register, onItemsClick } = this.props;
 
     const showItems = items.map((value) => (
-      <div className="my-1 mr-2" key={value}>
+      <div className="mt-1 mr-2" key={value}>
         <div className="bg-secondary text-white rounded-left-badge">
           {value}
         </div>
@@ -38,14 +44,16 @@ export class Items extends React.PureComponent {
       <React.Fragment>
         <InputGroup
           name={name}
+          type="text"
           label={label}
+          placeholder={placeholder}
           register={register}
           onClick={onItemsClick}
-          style="form-group col-12 px-0 mb-2"
+          style="col-12 px-0"
         />
         <CSSTransitionGroup
           component="div"
-          className="row col-12 px-3 mb-4"
+          className="row col-12 px-3"
           transitionName={{
             appear: "animate__animated",
             appearActive: "animate__fadeInDown",
@@ -57,7 +65,7 @@ export class Items extends React.PureComponent {
           transitionAppearTimeout={0}
           transitionLeaveTimeout={0}
         >
-          {showItems}
+          {items.length > 0 && showItems}
         </CSSTransitionGroup>
       </React.Fragment>
     );
