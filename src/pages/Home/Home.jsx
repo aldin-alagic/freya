ef7845537dc99ddb06cdda38d678adf1382a3fb9
aldin-alagic <aldin@dysordian.com>
2021-05-10 00:13:06 +0200

    Create Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Section from "../../components/Section/Section";
+
+const Home = () => {
+  return (
+      <Section title="Main">
+      </Section>
+  );
+}
+
+export default Home;
