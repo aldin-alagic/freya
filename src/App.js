3713abdbc7973f6a950b87c03670e51dc5c68b20
aldin-alagic <aldin@dysordian.com>
2021-05-10 00:15:39 +0200

    Add routing and app component to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
+
+import "./App.scss";
+import { Main } from "./lib/style/generalStyles";
 
 const App = () => {
-  return <h1>Hello</h1>;
+  return (
+      <ScrollToTop>
+        <Header />
+        <Main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Redirect to="/" />
+          </Switch>
+        </Main>
+        <Footer />
+      </ScrollToTop>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Router>,
+  document.getElementById("root")
 );
 
 reportWebVitals();
