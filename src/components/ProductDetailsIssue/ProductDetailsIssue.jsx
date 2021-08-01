06137d0009eec96c6318995d8d3496c165ce2724
aldin@dysordian.com <aldin@dysordian.com>
2021-08-01 21:36:26 +0200

    Add ProductDetailsIssue styled component

---
+++ src/components/ProductDetailsIssue/ProductDetailsIssue.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { ProductDetailsIssueWrapper } from './ProductDetailsIssueStyles';
+import { Keywords } from '../ProductCard/ProductCardStyles';
+import { ProductDetailsRow, ProductDetailsItemLabel, ProductDetailsItemValue, Pill, PillValue, HorizontalList } from '../../lib/style/generalStyles';
+
+const ProductDetailsIssue = ({ description, keywords }) => {
+	return (
+		<ProductDetailsIssueWrapper>
+			<ProductDetailsRow>
+				<ProductDetailsItemLabel>Description</ProductDetailsItemLabel>
+				<ProductDetailsItemValue>{description}</ProductDetailsItemValue>
+			</ProductDetailsRow>
+			<ProductDetailsRow>
+				<ProductDetailsItemLabel>Keywords</ProductDetailsItemLabel>
+				<ProductDetailsItemValue>
+					<HorizontalList>
+						{keywords &&
+							keywords.map((keyword) => (
+								<Pill key={keyword}>
+									<PillValue>{keyword}</PillValue>
+								</Pill>
+							))}
+					</HorizontalList>
+				</ProductDetailsItemValue>
+			</ProductDetailsRow>
+		</ProductDetailsIssueWrapper>
+	);
+};
+export default ProductDetailsIssue;
