3f6b3d27aaa40f5e08944ccaf8dbf39d096e2f3d
aldin@dysordian.com <aldin@dysordian.com>
2021-08-01 21:35:28 +0200

    Format theme.js

--- src/lib/style/theme.js
+++ src/lib/style/theme.js
@@ -1,32 +1,31 @@
 export const breakpoints = {
-  mobileLarge: 'min-width: 576px',
-  tablet: 'min-width: 768px',
-  desktop: 'min-width: 1024px',
-  desktopLarge: 'min-width: 1440px',
+	mobileLarge: 'min-width: 576px',
+	tablet: 'min-width: 768px',
+	desktop: 'min-width: 1024px',
+	desktopLarge: 'min-width: 1440px',
 };
 
 export const colors = {
-  blue: '#15698f',
-  darkBlue: '#145a8c',
-  black: '#333333',
-  darkBlack: '#34363b',
-  white: '#ffffff',
-  grey: '#cccccc',
-  darkGrey: '#bbbbbb',
-  lightGrey: '#f5f5f5',
-  shadow: '#777777',
-  lightshadow: '#ffffff9e',
-  blackOverlay: 'rgba(0, 0, 0, 0.3)',
-  green: '#28a745',
-  successBackground: '#dbf4e6',
-  errorBackground: '#ffd9d9',
-  error: '#eb5757',
-  success: '#27ae60',
+	blue: '#15698f',
+	darkBlue: '#145a8c',
+	black: '#333333',
+	darkBlack: '#34363b',
+	white: '#ffffff',
+	grey: '#cccccc',
+	darkGrey: '#bbbbbb',
+	lightGrey: '#f5f5f5',
+	shadow: '#777777',
+	lightshadow: '#ffffff9e',
+	blackOverlay: 'rgba(0, 0, 0, 0.3)',
+	green: '#28a745',
+	successBackground: '#dbf4e6',
+	errorBackground: '#ffd9d9',
+	error: '#eb5757',
+	success: '#27ae60',
 };
 
 export const boxShadow = '0px 0px 5px 0px rgb(0 0 0 / 10%)';
-export const strongBoxShadow = '1px 1px 6px 2px rgb(0 0 0 / 10%);';
-export const boxShadowHover =
-  '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)';
+export const strongBoxShadow = '0px 0px 3px 3px rgb(0 0 0 / 10%)';
+export const boxShadowHover = '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)';
 export const transitionCubic = 'all 0.3s cubic-bezier(.25,.8,.25,1)';
 export const transitionEase = 'all 0.3s ease-out';
