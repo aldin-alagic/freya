1678c2356416a6d85e4aacf6d40c0d364a177628
aldin.alagicc@gmail.com <aldin.alagicc@gmail.com>
2022-01-01 23:08:12 +0100

    Fix Login and Register styles

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,41 +1,34 @@
-import React, { useState } from "react";
-import * as Yup from "yup";
+import React, { useState } from 'react';
+import * as Yup from 'yup';
 
-import Section from "../../components/Section/Section";
+import Section from '../../components/Section/Section';
 import DataLoader from './../../components/DataLoader/DataLoader';
 
-import {
-  Title,
-  Form,
-  FormRow,
-  InputLabel,
-  InputText,
-  InputError,
-  Button,
-  SuccessMessage,
-} from "../../lib/style/generalStyles";
+import { Header, Title, Form, FormRow, InputLabel, InputText, InputError, Button, SuccessMessage } from '../../lib/style/generalStyles';
 
 const Login = () => {
-  return (
-    <>
-      <Title>Login</Title>
-      <Section withoutTopPadding>
-        <Form centered>
-          <FormRow>
-            <InputLabel htmlFor="email">Email</InputLabel>
-            <InputText id="email" type="email" />
-          </FormRow>
-          <FormRow>
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <InputText id="password" type="password" />
-          </FormRow>
-          <FormRow>
-            <Button type="submit">Login</Button>
-          </FormRow>
-        </Form>
-      </Section>
-    </>
-  );
+	return (
+		<>
+			<Section>
+				<Header isCentered>
+					<Title>Login</Title>
+				</Header>
+				<Form isCentered>
+					<FormRow>
+						<InputLabel htmlFor="email">Email</InputLabel>
+						<InputText id="email" type="email" />
+					</FormRow>
+					<FormRow>
+						<InputLabel htmlFor="password">Password</InputLabel>
+						<InputText id="password" type="password" />
+					</FormRow>
+					<FormRow>
+						<Button type="submit" isFullWidth>Login</Button>
+					</FormRow>
+				</Form>
+			</Section>
+		</>
+	);
 };
 
 export default Login;
